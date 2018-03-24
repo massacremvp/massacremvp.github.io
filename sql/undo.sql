f5a1b38bf512ec194446a46ac4b55eb173a703c6
Victor Gawk <victor.gawk@gmail.com>
2018-03-24 16:52:50 -0300

    mvp Gold Queen Scaraba

---
+++ sql/undo.sql
@@ -0,0 +1,8 @@
+drop table mining_map_guild;
+drop table mining_map;
+drop sequence mining_map_seq;
+drop table mvp_guild;
+drop table mvp_alias;
+drop table mvp;
+drop sequence mvp_seq;
+drop table guild;
